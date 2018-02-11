2338b908f34279b980aab05cedc5bf5c7fb9e5ec
Rauof M <abdelrauof197@gmail.com>
2018-02-11 04:30:01 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,36 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
   
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+  
+  def self.column_names
+    DB[:conn].results_as_hash
+    table_info = DB[:conn].execute("PRAGMA table_info('#{table_name}')")
+    columns=[]
+    table_info.each { |col|
+      columns << col["name"]
+    }
+    columns.compact
+  end
+  
+  def initialize(option= {})
+    option.each { |k,v|
+      self.send("#{k}=",v)
+    }
+  end
+
 end
+
+# learn --fail-fast
+
+
+
+
+
+
+
